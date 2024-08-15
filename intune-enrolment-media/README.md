9e59de595089d222f0e5d3a0d22cf676bcc616cc
contrxl <contrxl@protonmail.com>
2024-08-15 14:40:03 +0100

    Merged scripts into one.

--- intune-enrolment-media/README.md
+++ intune-enrolment-media/README.md
@@ -1,7 +1,7 @@
 # To-Do
 
 - [x] Add cleanup to end of script.
-- [ ] Change output directory from $TEMP to a permanent directory to access.
+- [x] Change output directory from $TEMP to a permanent directory to access.
 - [ ] Add validation logic to script.
 - [ ] Merge media creation & USB erasure script.
 - [ ] Make AutoPilot config injection optional.
--- intune-enrolment-media/media-creation-test.ps1
+++ intune-enrolment-media/media-creation-test.ps1
@@ -1,7 +1,47 @@
+## Bootable Meida
+## Start Instructions
+
+Clear-Host
+Write-Host " To begin, Connect a USB drive of at least 16GB size"
+Write-Host " It is recommended you remove all other USB drives!"
+Write-Host
+Write-Host " Checking for connected disks. This may take a moment..."
+Write-Host
+Write-Host " The selected disk will be completely erased!" -BackgroundColor White -ForegroundColor DarkMagenta 
+Write-Host " Any data will not be recoverable." -BackgroundColor White -ForegroundColor DarkMagenta 
+Write-Host " Ensure you do NOT format your boot drive!" -BackgroundColor White -ForegroundColor DarkMagenta 
+Write-Host " If you are unsure, press CTRL + C to abort." -BackgroundColor White -ForegroundColor DarkMagenta 
+
+## Write out connected disks.
+
+Get-Disk | Format-Table Number, Friendlyname, HealthStatus, PartitionStyle,@{n='Size';e={[int]($_.Size/1GB)}}
+
+## Display disk number and get confirmation of wipe.
+
+$DISKNUMBER = Read-Host -Prompt " Select a disk and press enter to confirm"
+Write-Host " WARNING! The following disk will be erased:" -ForegroundColor Red
+Get-Disk $DISKNUMBER | Format-Table Number, Friendlyname, HealthStatus, PartitionStyle,@{n='Size';e={[int]($_.Size/1GB)}}
+Write-Host " This cannot be undone!" -ForegroundColor Red
+
+$CONFIRMPROMPT = Read-Host -Prompt " Type 'YES' and press Enter to confirm"
+if ($CONFIRMPROMPT -ne "YES") {
+    Write-Host " Terminating process..."
+    Exit
+}
+
+## Wipe chosen USB
+Write-Host " Erasing disk..."
+Clear-Disk -Number $DISKNUMBER -RemoveData -Confirm:$false
+$BUILDSTICK = New-Partition -DiskNumber $DISKNUMBER -UseMaximumSize -AssignDriveLetter | Format-Volume -FileSystem NTFS
+$BUILDSTICK = ($BUILDSTICK | Get-Volume).DriveLetter
+$BUILDSTICK = ($BUILDSTICK + ":\")
+Write-Host " Disk formatted successfully!" -ForegroundColor Green
+Write-Host
+
 ## Initialize variables.
 
 $TEMPPATH = "C:\$(New-GUID)"
-$NEWISONAME = "$TEMPPATH\WindowAutopilot.iso"
+$NEWISONAME = "$TEMPPATH\WindowsAutopilot.iso"
 $ISOCONTENTS = "$TEMPPATH\iso\"
 $INSTALLWIM = "$ISOCONTENTS\sources\install.wim"
 $INSTALLWIMTEMP = "$TEMPPATH\installtemp.wim"
@@ -141,9 +181,15 @@ Write-Host " [+] $NEWISONAME successfully created!" -ForegroundColor Green
 ## Cleanup
 
 Write-Host " [*] Ejecting ISO."
-Dismount-DiskImage $ISO
-Write-Host " [+] ISO ejected." -ForegroundColor Green
-Write-Host
+if (Test-Path $ISO){ 
+    Dismount-DiskImage $ISO
+    Write-Host " [+] ISO ejected." -ForegroundColor Green
+    Write-Host
+} else {
+    Write-Host " [+] ISO not mounted. Continuing..." -ForegroundColor Green
+    Write-Host
+    Continue
+}
 
 Write-Host " [*] Dismounting install.wim."
 DISM /Unmount-WIM /mountdir:$MOUNTDIR /discard
@@ -151,20 +197,71 @@ Write-Host " [+] Image dismounted." -ForegroundColor Green
 Write-Host
 
 Write-Host " [*] Removing temporary install.wim."
-Remove-Item $INSTALLWIMTEMP
-Write-Host " [+] Removed." -ForegroundColor Green
-Write-Host
+if (Test-Path $INSTALLWIMTEMP) {
+    Remove-Item $INSTALLWIMTEMP
+    Write-Host " [+] Removed." -ForegroundColor Green
+    Write-Host
+} else {
+    Write-Host " [+] Temporary install.wim not found. Continuing..." -ForegroundColor Green
+    Write-Host
+    Continue
+}
 
 Write-Host " [*] Removing extracted ISO contents."
-Remove-Item $ISOCONTENTS -Recurse -Force
-Write-Host " [+] Removed." -ForegroundColor Green
-
+if (Test-Path $ISOCONTENTS) {
+    Remove-Item $ISOCONTENTS -Recurse -Force
+    Write-Host " [+] Removed." -ForegroundColor Green
+    Write-Host
+} else {
+    Write-Host " [+] ISO contents not found. Continuing..." -ForegroundColor Green
+    Write-Host
+    Continue
+}
+    
 Write-Host " [*] Removing mount folder."
-Remove-Item "$TEMPPATH\MOUNT" -Recurse -Force
-Write-Host " [+] Removed." -ForegroundColor Green
-
+if (Test-Path "$TEMPPATH\mount") {
+    Remove-Item "$TEMPPATH\mount" -Recurse -Force
+    Write-Host " [+] Removed." -ForegroundColor Green
+    Write-Host
+} else {
+    Write-Host " [+] Mount directory not found. Continuing..." -ForegroundColor Green
+    Write-Host
+    Continue
+}
+    
 Write-Host " [*] Removing oscdimg folder."
-Remove-Item "$TEMPPATH\oscdimg" -Recurse -Force
-Write-Host " [+] Removed." -ForegroundColor Green
+if (Test-Path "$TEMPPATH\oscdimg") {
+    Remove-Item "$TEMPPATH\oscdimg" -Recurse -Force
+    Write-Host " [+] Removed." -ForegroundColor Green        
+    Write-Host
+} else {
+    Write-Host " [+] OSCDIMG directory not found. Continuing..." -ForegroundColor Green
+    Write-Host
+    Continue
+}
 
 Write-Host " [+] ISO successfully built." -ForegroundColor Green
+
+$AUTOPILOTISO = "$TEMPPATH\WindowsAutopilot.iso"
+$MOUNTAUTOPILOT = Mount-DiskImage -ImagePath $AUTOPILOTISO -StorageType ISO -PassThru
+$MOUNTDRIVE = ($MOUNTAUTOPILOT | Get-Volume).DriveLetter
+$MOUNTDRIVE = ($MOUNTDRIVE + ":\")
+
+$FILES = Get-ChildItem -Path $MOUNTDRIVE -Recurse
+$FILECOUNT = $FILES.count
+Write-Host ""
+$i = 0
+Foreach ($FILE in $FILES) {
+    $i++
+    Write-Progress -activity "Copying files to USB..." -status "$FILE ($i of $FILECOUNT)" -percentcomplete (($i/$FILECOUNT)*100)
+    if ($FILE.psiscontainer) {
+        $SOURCEFILECONTAINER = $FILE.parent
+    } else {
+        $SOURCEFILECONTAINER = $FILE.directory
+    }
+    $RELATIVEPATH = $SOURCEFILECONTAINER.fullname.SubString($MOUNTDRIVE.length)
+    Copy-Item $FILE.fullname ($BUILDSTICK + $RELATIVEPATH) -Force
+}
+
+
+Write-Host "Drive built!" -ForegroundColor Green
